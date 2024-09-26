aa87763eaa20d289a6765d5143cb9de4ae299a80
PushkarOnGithub <pushkarsaini000@gmail.com>
2024-09-26 13:14:55 +0530

    test added for checking client user-agent

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const forwarded = req.headers['x-forwarded-for'];
     const ip = forwarded ? forwarded.split(',')[0] : req.connection.remoteAddress || req.socket.remoteAddress;
-  
-    res.status(200).json({ clientIp: ip });
+    const userAgent = req.headers['user-agent'];
+    res.status(200).json({ clientIp: ip, userAgent});
   }
